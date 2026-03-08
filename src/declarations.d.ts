3cf1b81a3b60893ba97988d657cd56bf138a6137
swiing <persovince@yahoo.fr>
2026-03-08 14:51:56 +0100

    chore: take into accound .frag and .vert extensions
    the `gl` dependency relies on GLSLangValidator, which expects those extensions
    to be used.

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,7 +1,12 @@
 // https://github.com/microsoft/TypeScript/issues/40694#issuecomment-697121163
 // https://github.com/microsoft/TypeScript/issues/9748#issuecomment-232822688
 
-declare module '*.glsl' {
-  const glsl: string
-  export default glsl
+declare module "*.vert" {
+  const glsl: string;
+  export default glsl;
+}
+
+declare module "*.frag" {
+  const glsl: string;
+  export default glsl;
 }
